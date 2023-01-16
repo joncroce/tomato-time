e0f69b8b3e50965a57c6b77bd9c94b1fa268d32a
Jon Croce <crocedev@gmail.com>
2023-01-16 03:47:06 -0500

    add 7-segment digital style clock

---
+++ src/components/Colon.svelte
@@ -0,0 +1,5 @@
+<svg class="colon" viewBox="0 0 40 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="20" cy="150" r="14.5" />
+	<circle cx="20" cy="90" r="14.5" />
+</svg>
+	
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,41 @@
-<main>
+<script lang="ts">
+  import Colon from '../components/Colon.svelte';
+	import SevenSegment from "../components/SevenSegment.svelte";
+</script>
 
+<main>
+	<section class="clock">
+		<SevenSegment />
+		<SevenSegment />
+		<Colon />
+		<SevenSegment />
+		<SevenSegment />
+	</section>
 </main>
 
 <style>
 	main {
+		display: flex;
+		justify-content: center;
+		align-items: stretch;
 		width: 100vw;
 		height: 100vh;
-		background-color: oklch(54.9% 0.191 35.235);
+		background-color: oklch(50% 0.19 35);
+	}
+
+	.clock {
+		display: grid;
+		grid-template-columns: 23.5% 23.5% 6% 23.5% 23.5%;
+	}
+
+	:global(svg) {
+		width: 100%;
+		height: 100%;
+	}
+
+	:global(svg > *) {
+		stroke-width: 2;
+		stroke: oklch(10% 0.19 145);
+		fill: oklch(50% 0.19 145);
 	}
 </style>
