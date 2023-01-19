4e38291326139bc3144e9e2899f43c0043429dd0
Jon Croce <crocedev@gmail.com>
2023-01-19 04:52:31 -0500

    make showProgressBar option default to true

--- src/stores/options.ts
+++ src/stores/options.ts
@@ -3,4 +3,4 @@ import { writable } from 'svelte/store';
 export const showLeadingZero = writable(false);
 export const twentyFourHourMode = writable(false);
 export const blinkingColon = writable(false);
-export const showProgressBar = writable(false);
+export const showProgressBar = writable(true);
