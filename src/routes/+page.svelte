a28d47819a0768d992d0bfa3e9203d75ae5fc911
Jon Croce <crocedev@gmail.com>
2024-02-25 13:45:46 -0500

    Implement persisting options to local storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,55 @@
   import OptionsMenu from "../components/OptionsMenu.svelte";
   import Clock from "../components/Clock.svelte";
   import Timer from "../components/Timer.svelte";
+  import { onMount } from "svelte";
+  import { optionsStore } from "../stores/options.svelte";
+
+  onMount(() => {
+    optionsStore.initialize();
+  });
 </script>
 
 <main>
-  <OptionsMenu />
-  <Clock />
-  <Timer />
+  <noscript
+    ><span
+      >Please enable JavaScript and refresh the page to use Tomato Time</span
+    ></noscript
+  >
+  {#if optionsStore.initialized}
+    <OptionsMenu />
+    <Clock />
+    <Timer />
+  {/if}
 </main>
 
-<style>
+<style lang="postcss">
   main {
     display: flex;
     flex-direction: column;
     align-items: stretch;
-    width: 100vw;
-    height: 100vh;
+    width: 100lvw;
+    height: 100lvh;
     background-color: var(--color-primary-dark);
   }
 
   :global(svg.seven-segment, svg.colon) {
     width: 100%;
     height: 100%;
+  }
+
+  noscript {
+    height: 100%;
+    width: 100%;
+    display: grid;
+    place-items: center;
+    font-size: 3rem;
+
+    span {
+      padding: 0.25em 0.5em;
+      border: 2px solid var(--color-tertiary-dark);
+      border-radius: 2em;
+      background-color: var(--color-primary-lighter);
+      color: var(--color-secondary-dark);
+    }
   }
 </style>
--- src/stores/options.svelte.ts
+++ src/stores/options.svelte.ts
@@ -1,26 +1,65 @@
 export enum Option {
-	SHOW_LEADING_ZERO,
-	SHOW_PROGRESS_BAR,
-	TWENTY_FOUR_HOUR_MODE
+	SHOW_LEADING_ZERO = "SHOW_LEADING_ZERO",
+	SHOW_PROGRESS_BAR = "SHOW_PROGRESS_BAR",
+	TWENTY_FOUR_HOUR_MODE = "TWENTY_FOUR_HOUR_MODE"
 }
 
-class OptionsStore {
-	showLeadingZero = $state<boolean>(false);
-	showProgressBar = $state<boolean>(true);
-	twentyFourHourMode = $state<boolean>(false);
+function getOptionFromLocalStorage(option: Option) {
+	return localStorage.getItem(option);
+}
 
-	constructor() { }
+function setOptionToLocalStorage(option: Option, value: boolean) {
+	localStorage.setItem(option, JSON.stringify(value));
+}
+
+const defaultOptions = new Map<string, boolean>(
+	[
+		[Option.SHOW_LEADING_ZERO, false],
+		[Option.SHOW_PROGRESS_BAR, true],
+		[Option.TWENTY_FOUR_HOUR_MODE, false]
+	]
+);
+
+class OptionsStore {
+	showLeadingZero = $state<boolean>(defaultOptions.get(Option.SHOW_LEADING_ZERO)!);
+	showProgressBar = $state<boolean>(defaultOptions.get(Option.SHOW_PROGRESS_BAR)!);
+	twentyFourHourMode = $state<boolean>(defaultOptions.get(Option.TWENTY_FOUR_HOUR_MODE)!);
+	initialized = $state(false);
+
+	constructor() {
+	}
+
+	initialize() {
+		const storedShowLeadingZero = getOptionFromLocalStorage(Option.SHOW_LEADING_ZERO);
+		if (storedShowLeadingZero) {
+			this.showLeadingZero = JSON.parse(storedShowLeadingZero);
+		}
+		const storedShowProgressBar = getOptionFromLocalStorage(Option.SHOW_PROGRESS_BAR);
+		if (storedShowProgressBar) {
+			this.showProgressBar = JSON.parse(storedShowProgressBar);
+		}
+
+		const storedTwentyFourHourMode = getOptionFromLocalStorage(Option.TWENTY_FOUR_HOUR_MODE);
+		if (storedTwentyFourHourMode) {
+			this.twentyFourHourMode = JSON.parse(storedTwentyFourHourMode);
+		}
+
+		this.initialized = true;
+	}
 
 	toggle(option: Option) {
 		switch (option) {
 			case Option.SHOW_LEADING_ZERO:
 				this.showLeadingZero = !this.showLeadingZero;
+				setOptionToLocalStorage(option, this.showLeadingZero);
 				break;
 			case Option.SHOW_PROGRESS_BAR:
 				this.showProgressBar = !this.showProgressBar;
+				setOptionToLocalStorage(option, this.showProgressBar);
 				break;
 			case Option.TWENTY_FOUR_HOUR_MODE:
 				this.twentyFourHourMode = !this.twentyFourHourMode;
+				setOptionToLocalStorage(option, this.twentyFourHourMode);
 				break;
 		}
 	}
